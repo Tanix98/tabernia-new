b2d36b0b0a87c837c093941db20ececdbb440168
Tanix98 <Tanix98@...>
2024-03-02 15:52:35 +0100

    replace search bar text button with icon

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -23,11 +23,17 @@ export default {
         <!-- Hamburger menu button -->
         <div @click="showMenu = !showMenu" class="flex ms-auto sm:hidden">
           <button type="button" class="text-gray-100 hover:text-gray-300">
-            <svg viewBox="0 0 24 24" class="w-7 h-7 fill-current">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              class="w-7 h-7"
+            >
               <path
                 fill-rule="evenodd"
-                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-              ></path>
+                d="M2 3.75A.75.75 0 0 1 2.75 3h10.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 3.75ZM2 8a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 8Zm0 4.25a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z"
+                clip-rule="evenodd"
+              />
             </svg>
           </button>
         </div>
@@ -35,7 +41,7 @@ export default {
       <!-- Hamburger menu -->
       <ul
         :class="showMenu ? 'flex' : 'hidden'"
-        class="flex-grow ms-auto sm:ms-0 text-right font-bold flex-col mt-2 mb-2 gap-4 sm:flex sm:flex-row sm:items-center sm:mt-0 sm:mb-0"
+        class="flex-grow text-lg text-right ms-auto sm:ms-0 flex-col mt-2 mb-2 gap-4 sm:flex sm:flex-row sm:items-center sm:mt-0 sm:mb-0"
       >
         <li class="text-gray-100 hover:text-blue-500">
           <RouterLink to="/" class="hover:underline focus:underline">Store</RouterLink>
@@ -47,12 +53,16 @@ export default {
           <RouterLink to="/checkout" class="text-gray-100 hover:text-blue-500"
             ><svg
               xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              class="bi bi-cart me-1 w-5 h-5"
-              viewBox="0 0 16 16"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
             >
               <path
-                d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
               />
             </svg>
           </RouterLink>
--- src/components/SearchProductsComp.vue
+++ src/components/SearchProductsComp.vue
@@ -14,13 +14,28 @@ export default {
 </script>
 
 <template>
-  <div class="mb-2 max-w-96 flex flex-nowrap">
+  <div class="relative mb-2 inline-block">
     <input
-      class="border-black border py-1 px-2 min-w-0"
+      class="flex border-black border py-1 px-2 pe-8 w-full"
       type="text"
       v-model="text"
       placeholder="Search"
     />
-    <button class="bg-black border text-white py-1 px-2" @click="searchProductList">Search</button>
+    <button class="absolute top-1/2 right-2 transform -translate-y-1/2" title="Search">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        class="w-5 h-5"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+        />
+      </svg>
+    </button>
   </div>
 </template>
